f616aa07e783d99268ad8354c604e54de6b9f0d0
yonghowfung <yhfung940217@gmail.com>
2023-02-21 18:34:02 +0100

    edited index

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import '../assets/stylesheets/application.scss';
+import App from './components/app'
+import '../assets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 if (root) {
-  root.render(<Hello name="World" />);
+  root.render(<App />);
 }
